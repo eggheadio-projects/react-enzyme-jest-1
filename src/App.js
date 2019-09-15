9ad5117270c6880e1be0706f0d751f450ded1e4a
Parker <parkerjlandon@gmail.com>
2019-09-15 15:48:06 -0700

    lesson 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// removed <img src={logo} className="App-logo" alt="logo" /> from line 10
+
+const Test = () => <div>Testing</div>
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Test />
     </div>
   );
 }
