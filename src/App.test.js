423ba9d1f458729c91c21c695de07a613c8e2b52
Parker <parkerjlandon@gmail.com>
2019-09-22 12:52:10 -0700

    lesson 6

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import toJson from 'enzyme-to-json'
 configure({ adapter: new Adapter() })
 
 describe('<App />', () => {
-  const wrapper = shallow(<App />)
   it('should contain 1 p element', () => {
+    const wrapper = shallow(<App />)
     expect(wrapper.find('[text="Some title"]').exists()).toBe(true)
+  })
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />)
+    expect(toJson(tree)).toMatchSnapshot()
   })
 })
